5c0480e7d1d804ae1579e9c94a86c736fd45ce56
swaraj89 <panigrahiswaraj89@gmail.com>
2022-08-22 17:18:23 +0530

    actions test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ContextProvider } from "./Context";
 import PositionSizer from "./PositionSizer";
-
 const App = () => {
   return <PositionSizer></PositionSizer>;
 };
